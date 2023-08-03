62bdbf5ac09df41f861a302f11cd696f6cb61fb6
SudeepaChekuri <98836806+SudeepaChekuri@users.noreply.github.com>
2023-08-03 20:27:48 +0530

    Update ecs.tf

--- ecs.tf
+++ ecs.tf
@@ -3,7 +3,7 @@ resource "aws_ecs_cluster" "test-cluster" {
 }
 
 data "template_file" "testapp" {
-  template = file("./templates/image/image.json")
+  template = ("./templates/image/image.json")
 
   vars = {
     app_image      = var.app_image
